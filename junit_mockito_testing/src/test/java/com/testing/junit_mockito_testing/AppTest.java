f5a1888ae04cff42947ed8fa68586d29bb3049ea
naman kumar singh <singhnaman320@gmail.com>
2023-04-27 16:29:25 +0530

    Java app testing

--- junit_mockito_testing/src/test/java/com/testing/junit_mockito_testing/AppTest.java
+++ junit_mockito_testing/src/test/java/com/testing/junit_mockito_testing/AppTest.java
@@ -1,7 +1,50 @@
 package com.testing.junit_mockito_testing;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /* Unit test for simple App. */
 
 public class AppTest {
    
+	// For all these two tests(test and secondTest) BeforeAll and AfterAll will run only once and all 
+	// other method will run every time
+	
+	@BeforeAll
+	static void init() {
+		System.out.println("Inside Before All");
+	}
+	
+	@BeforeEach
+	void show() {
+		
+		System.out.println("Inside Before Each");
+	}
+	
+	@Test
+	void test() {
+		
+		System.out.println("Inside Test");
+	}
+	
+	@Test
+	void secondTest() {
+		
+		System.out.println("Inside another Test");
+	}
+	
+	@AfterEach
+	void after() {
+		
+		System.out.println("Inside After Each");
+	}
+	
+	@AfterAll
+	static void destroy() {
+		
+		System.out.println("Inside After All");
+	}
 }
