1224e1e1b8518bb53f82e5f8aa01c81f7d2d941f
naman kumar singh <singhnaman320@gmail.com>
2023-04-27 19:19:07 +0530

    Java app testing

---
+++ junit_mockito_testing/src/main/java/com/testing/junit_mockito_testing/MathUtils.java
@@ -0,0 +1,21 @@
+package com.testing.junit_mockito_testing;
+
+public class MathUtils {
+
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	
+	int divisionOfIntegers(int a, int b) {
+		
+		return a/b;
+	}
+	
+	double divisionOfDouble(double a, double b) {
+		
+		return a/b;
+	}
+}
