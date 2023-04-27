7498ffe04b7101fce8c5803baa71f645565f1565
naman kumar singh <singhnaman320@gmail.com>
2023-04-28 00:01:34 +0530

    Added Test Cases

--- junit_mockito_testing/src/test/java/com/testing/junit_mockito_testing/MathUtilsTest.java
+++ junit_mockito_testing/src/test/java/com/testing/junit_mockito_testing/MathUtilsTest.java
@@ -17,6 +17,17 @@ class MathUtilsTest {
 		// 18, 7 -> 2
 		assertEquals(2, mUtils.divisionOfIntegers(18, 7));
 		
+		// 10, 0 -> Arithmetic Exception
+		assertThrows(ArithmeticException.class, () -> mUtils.divisionOfIntegers(10, 0));
+		
+		// 0, 0 -> Arithmetic Exception
+		assertThrows(ArithmeticException.class, () -> mUtils.divisionOfIntegers(0, 0));
+		
+		// -10, 2 -> 5
+		assertEquals(-2, mUtils.divisionOfIntegers(-10, 5));
+		
+		// -10, -2 -> 5
+		assertEquals(5, mUtils.divisionOfIntegers(-10, -2));
 		
 	}
 
